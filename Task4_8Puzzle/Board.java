cb20c7e730a46d9acc87c7918096e19c42c27ab8
Alexey <ralexeyr@gmail.com>
2018-04-08 22:54:30 +0300

    in progress

--- Task4_8Puzzle/Board.java
+++ Task4_8Puzzle/Board.java
@@ -1,8 +1,12 @@
 import edu.princeton.cs.algs4.StdOut;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 // http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
 
 public class Board {
+    // todo: rename to tiles
     private int[][] board;
     
     public Board(int[][] blocks) {
@@ -10,24 +14,7 @@ public class Board {
             throw new java.lang.IllegalArgumentException();
         }
         
-        this.board = blocks;
-    }
-
-    public int dimension() {
-        return board.length;
-    }
-
-    public int hamming() {
-        int num = 0;
-        for (int i = 0; i < dimension(); i++){
-            for (int j = 0; j < dimension(); j++){
-                int expectedNum = (i * dimension()) + (j+1);
-                if (board[i][j] != expectedNum) {
-                    num++;
-                }
-            }
-        }
-        return num;
+        this.board = clone2dArray(blocks);
     }
     
     private int abs(int n) {
@@ -41,23 +28,69 @@ public class Board {
     private class Point {
         private int x;
         private int y;
+        
+        public Point() {};
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public String toString() {
+            return String.format("(%d, %d)", x, y);
+        }
     }
     
     private Point findPosition(int n) {
-        Point res = new Point();
-        res.x = n / dimension();
-        res.y = n % dimension();
+        if (n == 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
         
+        Point res = new Point();
+        n--;
+        res.x = n / dimension();
+        res.y = (n % dimension());
+            
         return res;
+    }
+    
+    private Point findBlankSquare() {
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                if (board[i][j] == 0) {
+                    return new Point(i, j);
+                }
+            }
+        }
+        throw new java.lang.IllegalArgumentException("can't find blank square ('0' element)");
+    }
+
+    public int dimension() {
+        return board.length;
+    }
+
+    public int hamming() {
+        int num = 0;
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                int expectedNum = (i * dimension()) + (j+1);
+                if (board[i][j] != expectedNum) {
+                    // skip last element because it should be blank square
+                    if (i == dimension() - 1 && j == dimension() - 1) {
+                        continue;
+                    }
+                    num++;
+                }
+            }
+        }
+        return num;
     }
     
     public int manhattan() {
         int num = 0;
-        for (int i = 0; i < dimension(); i++){
-            for (int j = 0; j < dimension(); j++){
-                // todo: consider 0 element as 'empty'
-                // to introduce some flag to indicate wether 'empty' is passed and if it is then consider than 'switch'
-                if (block[i][j] = 0) continue;
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                if (board[i][j] == 0) continue;
                 
                 int expectedNum = (i * dimension()) + (j+1);
                 if (board[i][j] != expectedNum) {
@@ -70,12 +103,13 @@ public class Board {
     }
 
     public boolean isGoal() {
-        for (int i = 0; i < dimension(); i++){
-            for (int j = 0; j < dimension(); j++){
-                // todo: consider 'empty' block = 0
-                //if (board[i][j] == 0) continue;
-                
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
                 int expectedNum = (i * dimension()) + (j+1);
+                // last element should be '0' - blank square
+                if (i == dimension() - 1 && j == dimension() - 1) {
+                    expectedNum = 0;
+                }
                 if (board[i][j] != expectedNum) {
                     return false;
                 }
@@ -85,30 +119,128 @@ public class Board {
     }
 
     public Board twin() {
-        
+        Point a = null;
+        int[][] twin = clone2dArray(board);
+        for (int i = 0; i < dimension(); i++) {
+            for (int j = 0; j < dimension(); j++) {
+                if (twin[i][j] != 0) {
+                    if (a == null) {
+                        a = new Point(i, j);
+                    } else {
+                        int tmp = twin[a.x][a.y];
+                        twin[a.x][a.y] = twin[i][j];
+                        twin[i][j] = tmp;
+                        return new Board(twin);
+                    }
+                }
+            }
+        }
+        throw new java.lang.IllegalArgumentException("can't exchange 2 elements to get twin");
     }
 
     public boolean equals(Object y) {
-
+        if (y == this) {
+            return true;
+        }
+        if (y == null || y.getClass() != this.getClass()) {
+            return false;
+        }
+        
+        Board b = (Board) y;
+        return Arrays.deepEquals(this.board, b.board);
     }
-
-    public Iterable<Board> neighbors() {
-
-    }
-
-    public String toString() {
-        String result = "";
-        // pring dimention (i.e. 3)
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                result += board[i][j] + " ";
-            }
-            result += System.lineSeparator();
+    
+    // todo: need to override getHashCode()
+    
+    private int[][] clone2dArray(int[][] arr){
+        int [][] result = new int[arr.length][];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[i].clone();
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    public Iterable<Board> neighbors() {
+        List<Board> neighbors = new ArrayList<Board>();
+        //Board b = new Board(this.board.clone());
+        int n = dimension();
+        Point blank = findBlankSquare();
+        int x = blank.x;
+        int y = blank.y;
+        
+        if (x < n-1) {
+            int[][] b = clone2dArray(board);
+            b[x][y] = b[x+1][y];
+            b[x+1][y] = 0;
+            neighbors.add(new Board(b));
+        }
+        if (x > 0) {
+            int[][] b = clone2dArray(board);
+            b[x][y] = b[x-1][y];
+            b[x-1][y] = 0;
+            neighbors.add(new Board(b));
+        }
+        
+        if (y < n-1) {
+            int[][] b = clone2dArray(board);
+            b[x][y] = b[x][y+1];
+            b[x][y+1] = 0;
+            neighbors.add(new Board(b));
+        }
+        if (y > 0) {
+            int[][] b = clone2dArray(board);
+            b[x][y] = b[x][y-1];
+            b[x][y-1] = 0;
+            neighbors.add(new Board(b));
+        }
+        
+        return neighbors;
+    }
+    
+    public String toString() {
+        int n = dimension();
+        StringBuilder s = new StringBuilder();
+        s.append(n + System.lineSeparator());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append(System.lineSeparator());
+        }
+        return s.toString();
+    }
 
+    public static void main(String[] args) {
+        int[][] blocks = new int[3][3];
+        blocks[0][0] = 1;
+        blocks[0][1] = 0;
+        blocks[0][2] = 2;
+        blocks[1][0] = 3;
+        blocks[1][1] = 4;
+        blocks[1][2] = 5;
+        blocks[2][0] = 6;
+        blocks[2][1] = 7;
+        blocks[2][2] = 8;
+        
+        Board b = new Board(blocks);
+        StdOut.println(b.toString());
+        StdOut.println(b.twin());
+        StdOut.println(b.isGoal());
+        StdOut.println(b.hamming());
+        StdOut.println(b.manhattan());
+        /*
+        StdOut.println(b.findPosition(1));
+        StdOut.println(b.findPosition(2));
+        StdOut.println(b.findPosition(3));
+        StdOut.println(b.findPosition(4));
+        StdOut.println(b.findPosition(5));
+        StdOut.println(b.findPosition(6));
+        StdOut.println(b.findPosition(7));
+        StdOut.println(b.findPosition(8));
+        StdOut.println(b.findPosition(9));
+        */
+        for (Board i : b.neighbors()) {
+            StdOut.println(i.toString());
+        }
     }
 }
--- Task4_8Puzzle/Solver.java
+++ Task4_8Puzzle/Solver.java
@@ -1,21 +1,116 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solver {
+    private SearchNode solution;
+    private boolean isSolvable = false;
+    
+    private class SearchNode {
+        private Board board;
+        private int moves;
+        private SearchNode predecessor;
+        
+        public SearchNode(Board board, int moves, SearchNode prev) {
+            this.board = board;
+            this.moves = moves;
+            this.predecessor = prev;
+        }
+    }
+    
+    private Comparator<SearchNode> manhattanComparator() {
+        return new Comparator<SearchNode>() {
+            
+            public int compare(SearchNode node1, SearchNode node2) {
+                int a = node1.board.manhattan() + node1.moves;
+                int b = node2.board.manhattan() + node2.moves;
+                
+                if (a > b) {
+                    return 1;
+                }
+                if (a < b) {
+                    return -1;
+                }
+                return 0;
+            }
+        };
+    }
+    
+    private SearchNode step(MinPQ<SearchNode> queue) {
+        SearchNode node = queue.delMin();
+        if (node.board.isGoal()) {
+            return node;
+        }
+        for (Board b : node.board.neighbors()) {
+            queue.insert(new SearchNode(b, node.moves + 1, node));
+        }
+        return null;
+    }
+    
     public Solver(Board initial) {
-
+        if (initial == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        
+        MinPQ<SearchNode> queue = new MinPQ<SearchNode>(manhattanComparator());
+        // one more queue for board's twin
+        MinPQ<SearchNode> twinQueue = new MinPQ<SearchNode>(manhattanComparator());
+        
+        queue.insert(new SearchNode(initial, 0, null));
+        twinQueue.insert(new SearchNode(initial.twin(), 0, null));
+        while (true) {
+            SearchNode node = queue.delMin();
+            if (node.board.isGoal()) {
+                this.solution = node;
+                this.isSolvable = true;
+                break;
+            }
+            //StdOut(node.board.toString());
+            for (Board b : node.board.neighbors()) {
+                if (node.predecessor == null || !b.equals(node.predecessor.board)) {
+                    queue.insert(new SearchNode(b, node.moves + 1, node));
+                }
+            }
+            
+            // twin
+            SearchNode twinNode = twinQueue.delMin();
+            if (twinNode.board.isGoal()) {
+                this.isSolvable = false;
+                break;
+            }
+            for (Board b : twinNode.board.neighbors()) {
+                if (twinNode.predecessor == null || !b.equals(twinNode.predecessor.board)) {
+                    twinQueue.insert(new SearchNode(b, twinNode.moves + 1, twinNode));
+                }
+            }
+        }
     }
 
     public boolean isSolvable() {
-        throw new java.lang.IllegalArgumentException();
+        return isSolvable;
     }
 
     public int moves() {
-        throw new java.lang.IllegalArgumentException();
+        return solution.moves;
     }
 
     public Iterable<Board> solution() {
-        throw new java.lang.IllegalArgumentException();
+        if (isSolvable == false) {
+            return null;
+        } else {
+            List<Board> path = new ArrayList<Board>();
+            SearchNode curr = solution;
+            while (curr != null) {
+                path.add(curr.board);
+                curr = curr.predecessor;
+            }
+            Collections.reverse(path);
+            return path;
+        }
     }
 
     public static void main(String[] args) {
@@ -27,6 +122,8 @@ public class Solver {
             for (int j = 0; j < n; j++)
                 blocks[i][j] = in.readInt();
         Board initial = new Board(blocks);
+        StdOut.println(initial.twin());
+        StdOut.println(initial);
 
         // solve the puzzle
         Solver solver = new Solver(initial);
